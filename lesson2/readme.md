9bbed0b402be5109b9c6f7fede36e07447f04cf4
Shahed Ahmed Akkagi <sabuj.321@outlook.com>
2019-05-31 14:09:06 +0600

    lesson2 written tutorial update

---
+++ lesson2/readme.md
@@ -0,0 +1,2 @@
+# Lesson 2 - Go variable and constant
+*A variable is a storage unit of a particular type which has a name, storage address and value*
--- main.go
+++ main.go
@@ -1,9 +1,9 @@
 package main
 
-import (
-	"github.com/shahed321/ltw/lesson1"
-)
+import "fmt"
 
 func main() {
-	lesson1.Lesson1()
+	var myVal = 6.5
+
+	fmt.Println(myVal)
 }
